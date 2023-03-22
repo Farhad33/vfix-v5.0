1473b3874ce399e36f373ebbf4c4040e237d28b0
Paakraad <paakraad@Adels-MBP-2.attlocal.net>
2023-03-21 21:10:50 -0700

    cleaning up unused code

--- components/services/index.js
+++ components/services/index.js
@@ -12,8 +12,6 @@ export default function Services() {
     if (error) return <p>Error :(</p>
 
 
-    console.log("ser", data)
-
     return (
         <Container>
             <OverlapServices />
@@ -42,7 +40,6 @@ const OverlapServices = styled.div`
     background-color: #fff;
     transform: skewY(4deg);
     margin-bottom: -3rem;
-
 `
 
 const TopServices = styled.div`
@@ -68,39 +65,18 @@ const TabContainer = styled.div`
     width: auto;
     min-height: 500px;
 `
-const Tabs = styled.div`
-    margin: 2rem;
-    display: grid;
-    grid-template-areas:
-    'SmartHome'
-    'ComputerServices'
-    'HandymanServices';
-    @media only screen and (min-width: 600px) {
-        grid-template-areas:
-        'SmartHome ComputerServices HandymanServices';
-    }
+
+const Header = styled.h1`
+    margin-bottom: 2em;
 `
-const Tab = styled.h1`
-    border: 2px solid #4FC3F7;
-    border-radius: 10px;
-    text-align: center;
-    padding: 1.2em 0;
-    cursor: pointer;
-    margin: 1px;
-    font-size: 1.2rem;
-    font-weight: 400;
-    :hover {
-        background: #4FC3F7;
-    }
-    ${({ selected }) => selected && 'background: #4FC3F7;'}
-    grid-area: ${({ grid }) => grid };
-`
+
 const TabItems = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
 `
+
 const Item = styled.div`
     display: flex;
     flex-direction: column;
@@ -178,48 +154,3 @@ const Item = styled.div`
         }
     }
 `
-
-const Header = styled.h1`
-    margin-bottom: 2em;
-`
-
-
-
-const resources = [
-    
-    {
-        'title': 'Handyman Services',
-        'services': [
-            {
-                'icon': 'handyman-services/Board-Installation.svg',
-                'title': 'Board Installation',
-                'text': 'Our Experts are equipped with the right tools and adhere to proper safety precautions, so avoid a headache and have us install the board for you!'
-            },
-            {
-                'icon': 'handyman-services/Furniture-Assembly.svg',
-                'title': 'Furniture Assembly',
-                'text': 'Instead of dealing with weird instructions that nobody can understand, call us now for a quick quote. Our Experts can assemble even the most complicated furniture in less time and inspect them for safety. Now you can sit and relax while we take care of the job for you!'
-            },
-            {
-                'icon': 'handyman-services/Grill-Assembly.svg',
-                'title': 'Grill Assembly',
-                'text': 'Why put 4 hours of your valuable time to build a grill, when we can do it for you in less than an hour? We have all the necessary tools to assemble your heavy grill, quick and easy.'
-            },
-            {
-                'icon': 'handyman-services/Handyman-Services.svg',
-                'title': 'Handyman Services',
-                'text': 'VFix it, so you don’t have to! Let us take care of all the jobs that you don’t want to deal with! Our handyman services will cover assemblies and mounting'
-            },
-            {
-                'icon': 'handyman-services/TV-Mount-Service.svg',
-                'title': 'TV Mount Service',
-                'text': 'Stop aimlessly drilling holes into the wall just to find the stud! Have our Expert take care of it for you; we have professional tools to find the best position to secure your TV to the wall, hassle-free.'
-            }
-        ]
-    }
-]
-
-
-
-
-
