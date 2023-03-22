10193d8680d621b01111774cd4fd2caa181576b1
Paakraad <paakraad@Adels-MBP-2.attlocal.net>
2023-03-21 20:38:08 -0700

    changnging services to api strapi

--- components/services/index.js
+++ components/services/index.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import useFetch from '../../utility/hooks/useFetch'
+
 
 export default function Services() {
-    const [ tabIndex, setTabIndex ] = useState(1)
+    const [ tabIndex, setTabIndex ] = useState(0)
+
+    const { loading, error, data } = useFetch('http://strapi.myvfix.com/api/services?populate=articles,thumbnail');
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error :(</p>
+
+
+    console.log("ser", data)
+
     return (
         <Container>
             <OverlapServices />
@@ -10,22 +21,13 @@ export default function Services() {
             <TopServices />
             <Header>VFix Services</Header>
             <TabContainer>
-                <Tabs>
-                    {resources.map(({ title }, index) => (
-                        <Tab
-                            key={ index }
-                            onClick={ () => setTabIndex(index) }
-                            selected={ tabIndex === index }
-                            grid={ title.replace(/\s/g,'') }
-                        >{ title }</Tab>
-                    ))}
-                </Tabs>
                 <TabItems>
-                    {resources[tabIndex].services.map(({ title, text, icon }, index) => (
-                        <Item key={ index }>
+                    {data.map(({id, attributes: {title, description, thumbnail}}) => (
+                        <Item key={ id }>
                             <h2>{ title }</h2>
-                            <img src={`assets/services/${icon}`} />
-                            <p>{ text }</p>
+                            <p>{ description }</p>
+                            <img src={`http://strapi.myvfix.com${thumbnail.data.attributes.url}`} />
+
                         </Item>
                     ))}
                 </TabItems>
@@ -103,43 +105,31 @@ const Item = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    border: 1px solid gray;
+    border-radius: 15px;
+    margin: 10px;
     width: 100%;
     min-height: 10em;
     height: 15rem;
     padding-top: 1rem;
     img {
         height: 4em;
+        opacity: .4;
+        margin-top: -50px;
     }
     h2 {
         font-size: 1.2rem;
         font-weight: 400;
+        margin: 5px;
     }
     p {
-        opacity: 0;
+        margin: 5px;
         text-align: center;
         padding: 0 .5rem;
         width: 17rem;
+        font-weight: 400;
     }
-    :hover {
-        img {
-            transition-duration: 500ms;
-            opacity: .25;
-            height: 9em;
-            margin: 1rem;
-        }
-        p {
-            opacity: 1;
-            font-size: 1rem;
-            font-weight: 400;
-            position: absolute;
-            margin: 2.5rem;
-            transition-duration: 500ms;
-        }
-        h2 {
-            transition-duration: 300ms;
-            font-weight: bold;
-    }
-    }
+
     @media only screen and (min-width: 600px) {
         width: 15rem;
         img {
@@ -195,86 +185,7 @@ const Header = styled.h1`
 
 
 const resources = [
-    {
-        'title': 'Smart Home',
-        'services': [
-            {
-                'icon': 'smart-home/Artificial-Intelligence.svg',
-                'title': 'Artificial Intelligence',
-                'text': 'Let your voice command the house! Our VFix Experts can set up your Alexa, Google Home, and Home Pod to ensure that all your smart devices are at your service. Alexa, Turn on the TV!'
-            },
-            {
-                'icon': 'smart-home/Smart-Home-Setup.svg',
-                'title': 'Smart Home Setup',
-                'text': 'Connecting a bunch of smart devices to a home system can be difficult, but we make it easy. Let us connect and set up your devices to a single hub so that they can work seamlessly with each other.'
-            },
-            {
-                'icon': 'smart-home/Smart-Lighting.svg',
-                'title': 'Smart Lighting',
-                'text': 'We know how frustrating it can be when you’re already in bed, but you forgot to turn the lights off. But have no fear! Have our Expert set up your smart lights, so you can simply turn it off with your phone. It’s that easy!'
-            },
-            {
-                'icon': 'smart-home/Smart-Lock.svg',
-                'title': 'Smart Lock',
-                'text': 'We can replace your old door knob with a smart one and configure the auto lock options to ensure that all security features are in place. We make sure to keep your home safe while you\'re away!'
-            },
-            {
-                'icon': 'smart-home/Smart-Remote-Setup.svg',
-                'title': 'Smart Remote Setup',
-                'text': 'Can I have the remote for the TV? How about for the sound bar? Apple TV? Amazon Fire TV? Let our VFix Experts conveniently replace all of your home theatre devices\' remotes with 1 smart universal remote or your smart phone!'
-            },
-            {
-                'icon': 'smart-home/Smart-Thermostat.svg',
-                'title': 'Smart Thermostat',
-                'text': 'Save $150/month by using a smart thermostat. We can install and configure any thermostat, regardless of the brand. So avoid the hassle of dealing with the wires and complicated configurations and give us a call!'
-            }
-        ]
-    },
-    {
-        'title': 'Computer Services',
-        'services': [
-            {
-                'icon': 'computer-services/Virus-Removal.svg',
-                'title': 'Virus Removal',
-                'text': 'We’ll sweep and check your PC for viruses and eliminate any possible threats that could be slowing you down. We will also focus on maintaining your PC’s security by taking care of viruses early to avoid file loss and data corruption.'
-            },
-            {
-                'icon': 'computer-services/Computer-Diagnostic-Repair.svg',
-                'title': 'Diagnostic And Repair',
-                'text': 'Stay up and running with a computer checkup and allow us to troubleshoot any issues that might be preventing your PC from performing at an optimal level.'
-            },
-            {
-                'icon': 'computer-services/Electronic-Setup.svg',
-                'title': 'Electronic Setup',
-                'text': 'We provide hardware and software installation, while specifically focusing on ways to increase your PC’s efficiency and usability.'
-            },
-            {
-                'icon': 'computer-services/Printer-Setup.svg',
-                'title': 'Printer Setup',
-                'text': 'We know how frustrating and confusing it is to setup a wireless printer. Our experts will not only setup and configure your new printer, but they will also teach you how to use all of its features and basic functions.'
-            },
-            {
-                'icon': 'computer-services/Data-Backup.svg',
-                'title': 'Data Backup',
-                'text': 'You never think about data backup until it’s too late. We’ll help you securely backup and transfer your music, pictures and documents from your PC to the external storage you provide.'
-            },
-            {
-                'icon': 'computer-services/Wi-Fi Solutions.svg',
-                'title': 'Wifi Solutions',
-                'text': 'With our Wireless Networking solution, we will install the necessary equipment to connect the devices to your new network. Also we will determine the possible problems on your current network.'
-            },
-            {
-                'icon': 'computer-services/PC-MAC-support.svg',
-                'title': 'PC/Mac Support',
-                'text': 'Wondering if it’s time to upgrade your PC or MAC? If you need a little help assessing your technology, this is what we do. Tell us your priorities, preferences, and needs and we’ll put together a customized plan to help you get what you want out of your technology.'
-            },
-            {
-                'icon': 'computer-services/PC-Tuneup.svg',
-                'title': 'PC Tune-UP',
-                'text': 'We’ll adjust important settings and remove unwanted programs to get your PC feeling like new again'
-            }
-        ]
-    },
+    
     {
         'title': 'Handyman Services',
         'services': [
