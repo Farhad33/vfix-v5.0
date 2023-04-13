2472f91c1c9fec55af45a8dd59121950774f085a
Majid Rahimi <majid88rahimi@gmail.com>
2023-04-13 22:34:17 +0300

    Added Login page

---
+++ components/login/Login.js
@@ -0,0 +1,105 @@
+import { useState } from 'react'
+import axios from 'axios';
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+
+
+
+export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        axios
+        .post('http://strapi.myvfix.com/api/auth/local', {
+            identifier: username,
+            password: password,
+        })
+        .then(response => {
+            // Handle success.
+            console.log('Well done!');
+            console.log('User profile', response.data.user);
+            console.log('User token', response.data.jwt);
+            router.push('/admin')
+        })
+        .catch(error => {
+            // Handle error.
+            console.log('An error occurred:', error.response);
+        });
+
+
+    }
+
+    return (
+        <Background>
+            <FormContainer>
+                <Logo src='assets/logo/VFix-Logo-larg.svg' />
+                <Title>Welcome Back</Title>    
+                <SubTitle>Enter your credentials to access your account</SubTitle>    
+                <Form onSubmit={handleOnSubmit}>
+                    <TextField 
+                        type="text" 
+                        placeholder="Enter Your Username" 
+                        onChange={(e) => setUsername(e.target.value) }
+                        value={username}
+                    />
+                    <TextField 
+                        type="password" 
+                        placeholder="Enter Your Password" 
+                        onChange={(e) => setPassword(e.target.value) }
+                        value={password} 
+                    />
+                    <SignIn type="submit" value="Sign In" />
+                </Form>
+            </FormContainer>
+        </Background>        
+    )
+}
+
+const Background = styled.div`
+    height: 100vh;
+    background: rgb(0,153,255);
+    background: linear-gradient(180deg, rgba(0,153,255,1) 0%, rgba(233,199,255,1) 86%, rgba(255,255,255,1) 99%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const FormContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 90px 20px;
+    width: 700px;
+    background-color: white;
+    border-radius: 20px;
+`
+const Logo = styled.img`
+	cursor: pointer;
+	width: 10em;
+	height: 5em;
+`
+const Title = styled.h1`
+    margin: 15px;
+`
+const SubTitle = styled.h3`
+    margin-bottom: 20px;
+`
+const Form = styled.form`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
+const TextField = styled.input`
+    width: 400px;
+    margin-bottom: 20px;
+    padding: 10px;
+    `
+const SignIn = styled.input`
+    padding: 10px;
+    width: 400px;
+    background-color: #2c9cff;
+    color: white;
+`
---
+++ pages/admin.js
@@ -0,0 +1,5 @@
+// import Login from '../components/login/Login'
+
+export default function AdminPage() {
+    return <div>This is Admin Page</div>
+}
