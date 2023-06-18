ec677afb9fcb6a581bdbac009a7250ef95dfe100
Majid Rahimi <majid88rahimi@gmail.com>
2023-06-19 00:10:53 +0300

    Improved deployment and Added favIcon

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,16 @@ scp VFixWeb-5.tar.gz vfix:~
 rm VFixWeb-5.tar.gz
 
 ssh vfix << 'ENDSSH'
-rm -rf VFixWeb-5
-mkdir VFixWeb-5
-tar xf VFixWeb-5.tar.gz -C VFixWeb-5 2> /dev/null
+rm -rf VFixWeb-temp
+mkdir VFixWeb-temp
+tar xf VFixWeb-5.tar.gz -C VFixWeb-temp 2> /dev/null
 rm VFixWeb-5.tar.gz
-cd VFixWeb-5
+cd VFixWeb-temp
 find  . -name '._*' -exec rm {} \;
 npm i
 npm run build
 pm2 delete VFixWeb-5
+rm -rf VFixWeb-5
+mv VFixWeb-temp VFixWeb-5
 pm2 start npm --name "VFixWeb-5" -- start
 ENDSSH
-
--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,13 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="description" content="VFix provides onsite computer and handyman services such as TV Mounting, Furniture Assembly, Exercise Equipment Assembly wall hanging and more."></meta>
+          <meta name="keywords" content="VFix, VFix San Francisco, myvfix, Computer Support, Printer Setup, Wifi Solution, Virus Removal, Electronic Setup, PC Tune-Up, Data Backup, Artificial Intelligence, Smart Home Setup, Smart Lighting, Smart Lock, Smart Remote Setup, Smart Thermostat, Board Installation, Furniture Assembly, Grill Assembly, Handyman Services, TV Mount Service"></meta>
+          <link rel="shortcut icon" href="static/assets/logo/site-icon.png"></link>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet"></link>
+          <link rel="icon" type="image/x-icon" href="assets/favicons/favicon.ico"></link>
           <link href="https://cdn.zenbooker.com/widget/latest/zenbooker.css" rel="stylesheet"></link>
           <script src="https://cdn.zenbooker.com/widget/latest/zenbooker.js" type="text/javascript"></script>
           {this.props.styles} {/* styled-components' styles */}
