0d066f1e9821aee9379ebda0897563f09136b7e4
Majid Rahimi <majid88rahimi@gmail.com>
2023-06-23 11:59:54 -0700

    Made changes in deploy process

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ scp VFixWeb-5.tar.gz vfix:~
 rm VFixWeb-5.tar.gz
 
 ssh vfix << 'ENDSSH'
-rm -rf VFixWeb-temp
 mkdir VFixWeb-temp
 tar xf VFixWeb-5.tar.gz -C VFixWeb-temp 2> /dev/null
 rm VFixWeb-5.tar.gz
@@ -13,8 +12,10 @@ cd VFixWeb-temp
 find  . -name '._*' -exec rm {} \;
 npm i
 npm run build
-pm2 delete VFixWeb-5
+cd ..
 rm -rf VFixWeb-5
 mv VFixWeb-temp VFixWeb-5
+cd VFixWeb-5
+pm2 delete VFixWeb-5
 pm2 start npm --name "VFixWeb-5" -- start
 ENDSSH
